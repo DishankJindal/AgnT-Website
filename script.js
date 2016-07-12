a16e7e50263e9040df5ca75a6aebf4e304a76c0c
Dishank Jindal <dishankjindalitn@gmail.com>
2016-07-12 13:53:46 +0530

    Landscape responsive design is working
    99% website completed!! Will keep updating this website as per future
    needs.

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function start() {
-  document.getElementById('menu1').style.display="block";
-  document.getElementById('menu2').style.display="none";
-  document.getElementById('sidebar').style.display="none";
-  document.getElementById('email').display="none";
-}
-
 function disimg1() {
   document.getElementById('menu1').style.display="none";
   document.getElementById('menu2').style.display="block";
   document.getElementById('sidebar').style.display="block";
   document.getElementById('email').display="inline";
+  document.getElementById('menu2').src="img/menu2.png";
 }
 
 function disimg2() {
