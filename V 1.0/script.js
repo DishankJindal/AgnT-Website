e5b7a67a0865c24a96a26959f70e4ee8ec65ace4
Dishank Jindal <dishankjindalitn@gmail.com>
2016-07-25 00:27:23 +0530

    Fresh Start with newer version!!

--- V 1.0/script.js
+++ V 1.0/script.js
@@ -2,16 +2,13 @@ function disimg1() {
   document.getElementById('menu1').style.display="none";
   document.getElementById('menu2').style.display="block";
   document.getElementById('sidebar').style.display="block";
-  document.getElementById('email').display="block";
-  document.getElementById('sidebar').style.width="50vmin";
-  document.getElementById('sidebar').style.animation="sidebar 0.5s linear";
+  document.getElementById('email').display="inline";
+  document.getElementById('menu2').src="img/menu2.png";
 }
 
 function disimg2() {
   document.getElementById('menu1').style.display="block";
   document.getElementById('menu2').style.display="none";
-  document.getElementById('sidebar').style.width="0vmin";
+  document.getElementById('sidebar').style.display="none";
   document.getElementById('email').display="none";
-  document.getElementById('menu2').src="";
-  document.getElementById('sidebar').style.animation="sidebarend 0.5s linear";
 }
